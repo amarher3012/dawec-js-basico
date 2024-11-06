c44340cc17585403e535ca5046e05f8d70add924
Alejandro Martin Herrera <amarher3012@fp.iesromerovargas.com>
2024-11-06 19:02:41 +0100

    Add up to 8

--- parte-5/arrays.js
+++ parte-5/arrays.js
@@ -1,12 +1,12 @@
 // Ejercicio 1
 const weekDays = [
-  'domingo',
-  'lunes',
-  'martes',
-  'miercoles',
-  'jueves',
-  'viernes',
-  'sabado',
+    'domingo',
+    'lunes',
+    'martes',
+    'miercoles',
+    'jueves',
+    'viernes',
+    'sabado',
 ];
 
 const input = parseInt(prompt('Introduce un numero del 1 al 7')) - 1;
@@ -18,11 +18,11 @@ alert('Es ' + weekDays[input]);
 const booleans = ['Verdadero', 'Falso'];
 const input2 = parseInt(prompt('Introduce otro numero del 0 al 9'));
 const boolTest = (input) => {
-  if (input >= 0 || input <= 9) {
-    alert(booleans[0]);
-  } else {
-    alert(booleans[1]);
-  }
+    if (input >= 0 || input <= 9) {
+        alert(booleans[0]);
+    } else {
+        alert(booleans[1]);
+    }
 };
 
 boolTest(input2);
@@ -31,11 +31,11 @@ boolTest(input2);
 
 const nums = [2, 4, 5, 1, 2];
 const sumaLista = (arr) => {
-  let res = 0;
-  for (let i = 0; i < arr.length; i++) {
-    res += arr[i];
-  }
-  alert(res);
+    let res = 0;
+    for (let i = 0; i < arr.length; i++) {
+        res += arr[i];
+    }
+    alert(res);
 };
 
 sumaLista(nums);
@@ -45,12 +45,63 @@ sumaLista(nums);
 const nums2 = [];
 let count = 0;
 while (true) {
-  const input2 = parseInt(prompt('Introduce numeros'));
-  if (isNaN(input2) || input2 === 0) {
-    break;
-  }
-  nums2[count] = input2;
-  count++;
+    const input2 = parseInt(prompt('Introduce numeros'));
+    if (isNaN(input2) || input2 === 0) {
+        break;
+    }
+    nums2[count] = input2;
+    count++;
 }
 
-console.log(nums2);
+// console.log(nums2);
+
+// Ejercicio 5
+
+const nums3 = [3, 6, 1, 2, 3];
+const min = (arr, letter) => {
+    let lowest = Infinity; // No habra errores si por ejemplo lowest = 9999 y nums3 = [1, 4, 1000000]
+    let index = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < lowest) {
+            lowest = arr[i];
+            index = i;
+        }
+    }
+    if (letter === 'v') {
+        return lowest;
+    } else if (letter === 'i') {
+        return index;
+    }
+};
+
+console.log(min(nums3, 'i'));
+
+// Ejercicio 6
+
+const contains = (letter, arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === letter) {
+            return true;
+        }
+    }
+    return false;
+};
+
+console.log('contains: ', contains('r', ['e', 'd', 'r', 'f']));
+
+// Ejercicio 7
+
+const isVowel = (letter) => {
+    const vowels = ['a', 'e', 'o', 'u'];
+    for (let i = 0; i < vowels.length; i++) {
+        if (vowels[i] === letter.toLowerCase()) {
+            return true;
+        }
+    }
+    return false;
+};
+
+console.log('isVowel: ', isVowel('A'));
+
+// Ejercicio 8
+const arr = ['rojo', 'verde', 'azul'];
